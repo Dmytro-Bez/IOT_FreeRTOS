c0fc5fc0c82cd44c3d230eed8292d31ca5aaf8b1
Dmytriy <61518737+Dmytro-Bez@users.noreply.github.com>
2022-06-10 17:31:10 +0300

    Update files
    Added keys to connect mqtt

--- esp_led_mqtt_temp/main/main.c
+++ esp_led_mqtt_temp/main/main.c
@@ -4,6 +4,20 @@
 #include <esp_mqtt.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
+#include <string.h>
+#include <string.h>
+#include <ds18b20.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+
+#define TEMP_BUS 26                 // Temp Sensors are on GPIO26
+#define LED1 2
+#define LED2 15
+#define HIGH 1
+#define LOW 0
+#define digitalWrite gpio_set_level
 
 #define WIFI_SSID "EE"
 #define WIFI_PASS "EE@05kilogram"
@@ -17,6 +31,31 @@
 
 extern const uint8_t server_root_cert_pem_start[] asm("_binary_server_root_cert_pem_start");
 extern const uint8_t server_root_cert_pem_end[] asm("_binary_server_root_cert_pem_end");
+
+DeviceAddress tempSensors[2];
+
+/*Creating a ds18b20 temperature sensor data read function*/
+void getTempAddresses(DeviceAddress *tempSensorAddresses) {
+	unsigned int numberFound = 0;
+	reset_search();
+	while (search(tempSensorAddresses[numberFound],true)) {     // search for 2 addresses on the oneWire protocol
+		numberFound++;
+		if (numberFound == 2) break;
+	}
+	while (numberFound != 2) {                                  // if 2 addresses aren't found then flash the LED rapidly
+		numberFound = 0;
+		digitalWrite(LED2, HIGH);
+		vTaskDelay(100 / portTICK_PERIOD_MS);
+		digitalWrite(LED2, LOW);
+		vTaskDelay(100 / portTICK_PERIOD_MS);
+		reset_search();                                         // search in the loop for the temp sensors as they may hook them up
+		while (search(tempSensorAddresses[numberFound],true)) {
+			numberFound++;
+			if (numberFound == 2) break;
+		}
+	}
+	return;
+}
 
 // static void status_callback_aws(esp_mqtt_status_t status) {
 //     switch (status) {
@@ -81,6 +120,32 @@ static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_b
 }
 
 void app_main(void){
+    gpio_reset_pin(LED2);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(LED2, GPIO_MODE_OUTPUT);
+
+	ds18b20_init(TEMP_BUS);
+	// getTempAddresses(tempSensors);
+	// ds18b20_setResolution(tempSensors,2,10);
+
+	// printf("Address 0: 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x \n", tempSensors[0][0],tempSensors[0][1],tempSensors[0][2],tempSensors[0][3],tempSensors[0][4],tempSensors[0][5],tempSensors[0][6],tempSensors[0][7]);
+	// printf("Address 1: 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x \n", tempSensors[1][0],tempSensors[1][1],tempSensors[1][2],tempSensors[1][3],tempSensors[1][4],tempSensors[1][5],tempSensors[1][6],tempSensors[1][7]);
+
+	// while (1) {
+	// 	ds18b20_requestTemperatures();
+	// 	float temp1 = ds18b20_getTempF((DeviceAddress *)tempSensors[0]);
+	// 	float temp2 = ds18b20_getTempF((DeviceAddress *)tempSensors[1]);
+	// 	float temp3 = ds18b20_getTempC((DeviceAddress *)tempSensors[0]);
+	// 	float temp4 = ds18b20_getTempC((DeviceAddress *)tempSensors[1]);
+	// 	printf("Temperatures: %0.1fF %0.1fF\n", temp1,temp2);
+	// 	printf("Temperatures: %0.1fC %0.1fC\n", temp3,temp4);
+
+	// 	float cTemp = ds18b20_get_temp();
+	// 	printf("Temperature: %0.1fC\n", cTemp);
+    //     // ESP_LOGW("Main", "Temperature: %0.1fC\n", cTemp);
+	// 	vTaskDelay(1000 / portTICK_PERIOD_MS);
+	// }
+
     ESP_ERROR_CHECK(nvs_flash_init());  // initialize NVS flash
     // printf("Initialize NVS flash.\n");
     ESP_LOGW("Main", "Initialize NVS flash."); 
@@ -125,7 +190,6 @@ void app_main(void){
     // printf("Start Wi-Fi.\n");
     ESP_LOGW("Main", "Start Wi-Fi.");
 
- 
     // esp_mqtt_init(status_callback_aws, message_callback_aws, 256, 2000);   // initialize mqtt
     // printf("Initialize mqtt.\n");
 
