e8cda8275deb1733c12159d9d2c90991653197d9
Dmytriy <61518737+Dmytro-Bez@users.noreply.github.com>
2022-08-17 17:31:16 +0300

    Update functions
    Added function

--- Test_json_led_ds18d20/main/funk.c
+++ Test_json_led_ds18d20/main/funk.c
@@ -32,9 +32,16 @@ float temperature = 0;
 int msg_id;
 TaskHandle_t xPublishTask;
 TaskHandle_t xSensorTask;
-TaskHandle_t xSensor_Control;
+TaskHandle_t xLedTask;
+
+QueueHandle_t xSensor_Control = 0;
+QueueHandle_t xLed_Control = 0;
+
 char *my_json_string = NULL;
+char *pr_led = NULL;
+
 esp_mqtt_client_handle_t client;
+cJSON *root;
 
 extern const uint8_t client_cert_pem_start[] asm("_binary_client_crt_start");
 extern const uint8_t client_cert_pem_end[] asm("_binary_client_crt_end");
@@ -45,18 +52,12 @@ extern const uint8_t server_cert_pem_end[] asm("_binary_mosquitto_org_crt_end");
 
 static void blinky1(void *pvParameter){
     gpio_pad_select_gpio(BLINK_GPIO1);
-    /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO1, GPIO_MODE_OUTPUT);
     while(1) {
-        /* Blink off (output low) */
         gpio_set_level(BLINK_GPIO1, 0);
-        // printf("Led1 LOW\n");
-        
-        vTaskDelay(500 / portTICK_RATE_MS);
-        /* Blink on (output high) */
+        vTaskDelay(1500 / portTICK_RATE_MS);
         gpio_set_level(BLINK_GPIO1, 1);
-        // printf("Led1 HIGH\n");
-        vTaskDelay(500 / portTICK_RATE_MS);
+        vTaskDelay(1500 / portTICK_RATE_MS);
     }
 }
 
@@ -69,18 +70,18 @@ static void ds18b20_start(void *pvParameter){
     while (1){
         if (ds18b20_get_temperature(&temperature, NULL) == true) {
             printf("Temp:%0.1f\n", temperature);
-            cJSON *root = cJSON_CreateObject();
+            root = cJSON_CreateObject();
             cJSON_AddNumberToObject(root, "Temp:", temperature);
             
             my_json_string = cJSON_Print(root);
             ESP_LOGW(TAG, "\n%s",my_json_string);
             cJSON_Delete(root);
+            vTaskDelay(pdMS_TO_TICKS(2000));
         } else {
             printf("Not temp!\n");
         }
-        vTaskDelay(5000 / portTICK_RATE_MS);
+        // vTaskDelay(pdMS_TO_TICKS(2000));
     }
-   return 0;
 }
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data){
@@ -89,7 +90,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        ESP_LOGW(TAG, "MQTT_EVENT_CONNECTED");
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
@@ -98,7 +99,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED");
         break;
     case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED");
+        ESP_LOGW(TAG, "MQTT_EVENT_PUBLISHED");
         break;
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
@@ -123,53 +124,52 @@ static void mqtt_app_start(void){
     esp_mqtt_client_start(client);
 }
 
-static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *even_data){
-    switch (event_id){
-        case WIFI_EVENT_STA_START:
-            printf("Wifi connecting...\n");
-            break;
-        case WIFI_EVENT_STA_CONNECTED:
-            printf("Wifi connected...\n");
-            break;
-        case WIFI_EVENT_STA_DISCONNECTED:
-            printf("Wifi lost connection...\n");
-            break;
-        case IP_EVENT_STA_GOT_IP:
-            printf("Wifi got IP...\n\n");
-            break;
-        default:
-            break;
-    }
-}
+// static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *even_data){
+//     switch (event_id){
+//         case WIFI_EVENT_STA_START:
+//             printf("Wifi connecting...\n");
+//             break;
+//         case WIFI_EVENT_STA_CONNECTED:
+//             printf("Wifi connected...\n");
+//             break;
+//         case WIFI_EVENT_STA_DISCONNECTED:
+//             printf("Wifi lost connection...\n");
+//             break;
+//         case IP_EVENT_STA_GOT_IP:
+//             printf("Wifi got IP...\n\n");
+//             break;
+//         default:
+//             break;
+//     }
+// }
 
 static void connect_wifi(void){
-    esp_netif_init();                                                       //TCP-IP init
-    esp_event_loop_create_default();                                        //Event loop
-    esp_netif_create_default_wifi_sta();                                    //Wifi station
-    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&wifi_initiation);                                        //Config Phase wifi
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
-    wifi_config_t wifi_configuration = {
+    tcpip_adapter_init();
+    wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    wifi_config_t sta_wifi_config = {
         .sta = {
-            .ssid = SSID,
+            .ssid = SSID, 
             .password = PASS
-        }
+        },
     };
-    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
-    esp_wifi_start();                                                       //Wifi start phase
-    esp_wifi_connect();                                                    //Wifi connect phase
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_wifi_config));
+    
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
+    ESP_LOGI(TAG, "Connect to WiFi.");
 }
 
 static void publish_mqtt(void *pvParameter){
     while (1){
-        ESP_LOGI(TAG, "Sending data to topic %s...", topic_mqtt_data);
-        if(!xQueueReceive(xSensor_Control, &my_json_string, 3000)){
-            ESP_LOGI(TAG, "Error send dates sensor.\n");
+        ESP_LOGW(TAG, "Sending data to topic %s...", topic_mqtt_data);
+        if(xQueueReceive(xSensor_Control, &my_json_string, 6000)){
+            ESP_LOGW(TAG, "Error send dates sensor.\n");
         }
-
         esp_mqtt_client_publish(client, topic_mqtt_data, my_json_string, 0, 0, 0);
         free(my_json_string);
     }
-    vTaskDelay(5000 / portTICK_RATE_MS);
+//    vTaskDelay(5000 / portTICK_RATE_MS);
+vTaskDelay(pdMS_TO_TICKS(2000));
 }
--- Test_json_led_ds18d20/main/main.c
+++ Test_json_led_ds18d20/main/main.c
@@ -6,21 +6,26 @@ void app_main(void){
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     connect_wifi();
+
     mqtt_app_start();
-    
+    vTaskDelay(5000 / portTICK_RATE_MS);
+
     xSensor_Control = xQueueCreate(10, sizeof(int));
     if(xSensor_Control == NULL) ESP_LOGI(TAG, "Error xQueueCreate.\n");
     if(xTaskCreate(&publish_mqtt, "Publish_mqtt.", configMINIMAL_STACK_SIZE + 4096,NULL,5,xPublishTask) != pdTRUE){
-        ESP_LOGE("Error", "Error publish \n");
+        ESP_LOGI("Error", "Error publish \n");
         while(1);
     }
-    // xTaskCreate(&blinky1, "blinky1", 2048,NULL,5,NULL);   
-    if(xTaskCreate(&ds18b20_start, "Temp:/n", configMINIMAL_STACK_SIZE + 4096,NULL,5,xSensorTask) != pdTRUE){
+     
+    if(xTaskCreate(&ds18b20_start, "Temp:\n", configMINIMAL_STACK_SIZE + 4096,NULL,5,xSensorTask) != pdTRUE){
         ESP_LOGE("Error", "Error sensor \n");
         while(1);
     }
-
+        
+    xTaskCreate(&blinky1, "blinky1", 2048,NULL,5,NULL);  
+    
     while (true){
-        vTaskDelay(pdMS_TO_TICKS(3000));
+        ESP_LOGW(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
